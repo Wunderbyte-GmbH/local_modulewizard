16ec05e9d4f9a0f3af4a1befb6173f7d3fb987cf
Georg Maißer <georg.maisser@wunderbyte.at>
2021-09-27 12:05:14 +0200

    Version 1.0.2. (2021092700)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_modulewizard';
-$plugin->release = '1.0.1';
-$plugin->version = 2021092100;
+$plugin->release = '1.0.2';
+$plugin->version = 2021092700;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_STABLE;
